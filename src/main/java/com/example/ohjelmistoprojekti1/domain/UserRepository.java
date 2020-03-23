55c73898e89a0137c9dae47532bc362336ec41d2
Silja Wisse <silja.wisse@myy.haaga-helia.fi>
2020-03-23 18:58:09 +0200

    joo

--- src/main/java/com/example/ohjelmistoprojekti1/domain/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ohjelmistoprojekti1.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-        User findByUsername(String username);
-}
