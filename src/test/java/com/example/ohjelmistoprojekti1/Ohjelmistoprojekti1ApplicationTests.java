3e9726f021ef59ed1a1e8252b02e7a06254a0b2c
Silja Wisse <silja.wisse@myy.haaga-helia.fi>
2020-04-21 20:38:15 +0300

    usertesti

--- src/test/java/com/example/ohjelmistoprojekti1/Ohjelmistoprojekti1ApplicationTests.java
+++ src/test/java/com/example/ohjelmistoprojekti1/Ohjelmistoprojekti1ApplicationTests.java
@@ -4,6 +4,8 @@ package com.example.ohjelmistoprojekti1;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.ohjelmistoprojekti1.domain.classes.User;
+
 @SpringBootTest
 class Ohjelmistoprojekti1ApplicationTests {
 
---
+++ src/test/java/com/example/ohjelmistoprojekti1/UserTest.java
@@ -0,0 +1,41 @@
+package com.example.ohjelmistoprojekti1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.ohjelmistoprojekti1.domain.classes.User;
+import com.example.ohjelmistoprojekti1.domain.repositories.UserRepository;
+import com.example.ohjelmistoprojekti1.domain.repositories.UserTypeRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserTest {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private UserTypeRepository userTypeRepo;
+	
+	@Test
+	//testaa uuden käyttäjän lisäämisen
+		public void NewUser() {
+		
+		User user = new User();
+		user.setFirstname("Testi");
+		user.setLastname("Testaaja");
+		user.setPhonenumber("0419632587");
+		user.setEmail("Testi.testaaja@testi.com");
+		user.setUsername("TestaajaT");
+		user.setPassword("testaus");
+		user.setUsertype(userTypeRepo.findByUsertype("admin").get(0));
+		userRepo.save(user);		
+		
+		assertEquals(user.getFirstname(), "Testi");
+	}
+}
