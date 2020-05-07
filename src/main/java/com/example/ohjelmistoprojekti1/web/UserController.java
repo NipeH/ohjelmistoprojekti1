5915a58bcb3e809612bbcf02c400081b8f02d2c7
Justus Keinänen <justuskeinanen@gmail.com>
2020-05-07 23:34:18 +0300

    Add new endpoint for deleting users

--- src/main/java/com/example/ohjelmistoprojekti1/web/UserController.java
+++ src/main/java/com/example/ohjelmistoprojekti1/web/UserController.java
@@ -1,28 +1,19 @@
 package com.example.ohjelmistoprojekti1.web;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
-import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.example.ohjelmistoprojekti1.domain.classes.Event;
-import com.example.ohjelmistoprojekti1.domain.classes.Order;
-import com.example.ohjelmistoprojekti1.domain.classes.Ticket;
-import com.example.ohjelmistoprojekti1.domain.classes.TicketType;
 import com.example.ohjelmistoprojekti1.domain.classes.User;
 import com.example.ohjelmistoprojekti1.domain.classes.UserType;
 import com.example.ohjelmistoprojekti1.domain.repositories.UserRepository;
@@ -129,6 +120,18 @@ public class UserController {
 
 		}
 	}
-	
+	@PreAuthorize("hasAuthority('admin')") 
+	@DeleteMapping(value = "api/users/{userid}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void deleteUser(@PathVariable Long userid) {
+		try {
+			urepo.deleteById(userid);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+	}
+
 
 }
