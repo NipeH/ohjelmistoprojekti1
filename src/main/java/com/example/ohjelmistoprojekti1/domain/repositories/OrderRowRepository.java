c90949386f9ef5db6cbb34f22650fc8885a1808a
Silja <60481213+Silja1980@users.noreply.github.com>
2020-05-10 21:23:11 +0300

    Delete OrderRowRepository.java

--- src/main/java/com/example/ohjelmistoprojekti1/domain/repositories/OrderRowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ohjelmistoprojekti1.domain.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.ohjelmistoprojekti1.domain.classes.OrderRow;
-
-public interface OrderRowRepository extends CrudRepository<OrderRow, Long> {
-
-}
