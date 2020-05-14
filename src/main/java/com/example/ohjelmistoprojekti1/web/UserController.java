4e7f6a019ae330646c4ebf279690cdc86f3ed75b
Justus Keinänen <justuskeinanen@gmail.com>
2020-05-14 17:44:24 +0300

    nimen muutos patch

--- src/main/java/com/example/ohjelmistoprojekti1/web/UserController.java
+++ src/main/java/com/example/ohjelmistoprojekti1/web/UserController.java
@@ -1,12 +1,17 @@
 package com.example.ohjelmistoprojekti1.web;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,69 +24,62 @@ import com.example.ohjelmistoprojekti1.domain.classes.UserType;
 import com.example.ohjelmistoprojekti1.domain.repositories.UserRepository;
 import com.example.ohjelmistoprojekti1.domain.repositories.UserTypeRepository;
 
-
-
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository urepo;
 
 	@Autowired
 	private UserTypeRepository utrepo;
-	
-	
-	//Vain admin voi lisätä käyttäjiä
-	@PreAuthorize("hasAuthority('admin')") 
+
+	// Vain admin voi lisätä käyttäjiä
+	@PreAuthorize("hasAuthority('admin')")
 	// Uusi käyttäjä
 	@PostMapping(value = "/api/users")
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public User user(@RequestBody Map<String, Object> userinfo) {
 
 		BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
-		
+
 		try {
-		
 
-				String firstname = String.valueOf(userinfo.get("firstname"));
+			String firstname = String.valueOf(userinfo.get("firstname"));
 
-				String lastname = String.valueOf(userinfo.get("lastname"));
-	
-				String phone = String.valueOf(userinfo.get("phone"));
+			String lastname = String.valueOf(userinfo.get("lastname"));
 
-				String email = String.valueOf(userinfo.get("email"));
-	
-				String username = String.valueOf(userinfo.get("username"));
-		
-				String password = String.valueOf(userinfo.get("password"));
-				String hash = bc.encode(password);
+			String phone = String.valueOf(userinfo.get("phone"));
 
-			
-				String usertype = String.valueOf(userinfo.get("usertype"));
+			String email = String.valueOf(userinfo.get("email"));
 
-				UserType type = utrepo.findByUsertype(usertype).get(0);			
-				
-				User user = new User();
-				user.setFirstname(firstname);
-				user.setLastname(lastname);
-				user.setEmail(email);
-				user.setPhonenumber(phone);
-				user.setUsername(username);
-				user.setPassword(hash);
-				user.setUsertype(type);
-				urepo.save(user);
-				
-									
+			String username = String.valueOf(userinfo.get("username"));
+
+			String password = String.valueOf(userinfo.get("password"));
+			String hash = bc.encode(password);
+
+			String usertype = String.valueOf(userinfo.get("usertype"));
+
+			UserType type = utrepo.findByUsertype(usertype).get(0);
+
+			User user = new User();
+			user.setFirstname(firstname);
+			user.setLastname(lastname);
+			user.setEmail(email);
+			user.setPhonenumber(phone);
+			user.setUsername(username);
+			user.setPassword(hash);
+			user.setUsertype(type);
+			urepo.save(user);
 
 			return user;
-			
+
 		} catch (Exception e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-					"Käyttäjä on jo olemassa", e);
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Käyttäjä on jo olemassa", e);
 
 		}
 	}
-	@PreAuthorize("hasAuthority('admin')") 
+
+	@PreAuthorize("hasAuthority('admin')")
 	@DeleteMapping(value = "api/users/{userid}")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void deleteUser(@PathVariable Long userid) {
@@ -94,5 +92,26 @@ public class UserController {
 		}
 	}
 
+	@PreAuthorize("hasAuthority('admin')")
+	@PatchMapping(value = "api/users/{userid}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public User editUser(@PathVariable Long userid, Map<String, String> props) {
+		try {
+			User u = urepo.findById(userid).get();
+			props.keySet().stream().map((key) -> key.trim().toLowerCase());
+			if (props.keySet().contains("firstname")) {
+				u.setFirstname(props.get("firstname"));
+			}
+			if (props.keySet().contains("lastname")) {
+				u.setLastname(props.get("lastname"));
+			}
+
+			return u;
+
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+
+	}
 
 }
